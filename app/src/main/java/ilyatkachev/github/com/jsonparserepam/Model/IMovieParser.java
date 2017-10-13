c9e0a51ffd1e2e78fe980fb12822cd6410ce6e0a
Ilya Tkachev <ilya.tkachev97@gmail.com>
2017-10-13 23:04:20 +0300

    Added Movie and MovieList parser interfaces

---
+++ app/src/main/java/ilyatkachev/github/com/jsonparserepam/Model/IMovieParser.java
@@ -0,0 +1,8 @@
+package ilyatkachev.github.com.jsonparserepam.Model;
+
+
+public interface IMovieParser {
+
+    IMovie parse() throws Exception;
+
+}
---
+++ app/src/main/java/ilyatkachev/github/com/jsonparserepam/Model/IMoviesListParser.java
@@ -0,0 +1,8 @@
+package ilyatkachev.github.com.jsonparserepam.Model;
+
+
+public interface IMoviesListParser {
+
+    IMoviesList parse() throws Exception;
+
+}
